f3896a69870bb9558395eb1ac0ff0f09e9373733
jwolskyDLink <James.Wolsky@dlinkcorp.com>
2020-03-04 17:49:56 +0800

    3-4-2020 update on some file. continueing on bootstrap

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ __author__ = 'jrReubin'
 
 from flask import Flask, render_template, request, session
 from src.common.database import Database
+from src.models.blog import Blog
 from src.models.user import User
 
 app = Flask(__name__)
@@ -43,6 +44,25 @@ def register_user():
     User.register(email, password)
 
     return render_template("profile.html", email=session['email'])
+
+@app.route('/blogs/<string:user_id>')
+@app.route('/blogs')
+def user_blogs(user_id=None):
+    if user_id is not None:
+        user = User.get_by_id(user_id)
+    else:
+        user = User.get_by_email(session['email'])
+    blogs = user.get_blogs()
+
+    return render_template("user_blogs.html", blogs=blogs, email=user.email)
+
+@app.route('/posts/<string:blog_id>')
+def blog_posts(blog_id):
+    blog = Blog.from_mongo(blog_id)
+    posts = blog.get_posts()
+
+    return render_template('posts.html', posts=posts, blog_title=blog.title)
+
 
 if __name__ == '__main__':
     app.run(port=4995)
